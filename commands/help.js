1cf02e53fea888fad0217c2200a5a407e398095c
yutarou12 <52701144+yutarou12@users.noreply.github.com>
2021-03-06 10:42:22 +0900

    [Fix] bug fix

--- commands/help.js
+++ commands/help.js
@@ -9,13 +9,10 @@ exports.run = async(client, message, args) => {
                     name: "Bot", value: "`help, ping, info, invite`"
                 },
                 {
-                    name: "MCServers", value: "`list, clist, onlines, search`"
+                    name: "MCServers", value: "`list, clist, online, search`"
                 },
                 {
                     name: "`mc!list`が動かない場合", value: "権限が足りてない可能性がありますので\nBotを入れなおしてみてください。"
-                },
-                {
-                    name: "お知らせ", value: "サブ機能としてメッセージ展開機能を実装しました。"
                 }
             ],
             footer:{
