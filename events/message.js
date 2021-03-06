a9837b2c478807c25ca26620b8c3f4c373c10157
yutarou12 <52701144+yutarou12@users.noreply.github.com>
2021-03-06 10:25:47 +0900

    [Fix] Missing Permission の解決

--- events/message.js
+++ events/message.js
@@ -4,6 +4,15 @@ exports.run = async(client, message) => {
     if(!message.guild) return;
     if (message.author.bot) return;
     if (message.content.indexOf(config.prefix) !== 0) return;
+      
+    if(!message.guild.members.cache.get(client.user.id).hasPermission("ADD_REACTIONS") 
+    || !message.guild.members.cache.get(client.user.id).hasPermission("EMBED_LINKS") 
+    || !message.guild.members.cache.get(client.user.id).hasPermission("MANAGE_MESSAGES")
+    || !message.guild.members.cache.get(client.user.id).hasPermission("SEND_MESSAGES")) return message.channel.send({
+        embed:{
+            description:"コマンドを使用するには以下の権限が必要です\n```\n・メッセージの送信\n・メッセージの管理\n・リアクションの追加\n・埋め込みリンク\n```"
+        }
+    })
     
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();  
