b433a611018b091c66542aaba739c7d43a0bb449
yutarou12 <52701144+yutarou12@users.noreply.github.com>
2021-03-06 10:45:36 +0900

    [Fix] ステータスメッセージの変更

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const discord = require('discord.js');
 const config = require('../config.json');
 exports.run = (client) => {
     console.log(`[ Node: ${process.version} | Discord.js: ${discord.version}]\nConnected as: ${client.user.username} (ID: ${client.user.id})`);
-    client.user.setActivity(` ONLINE | 新機能実装(詳しくはhelpに) | ${config.prefix}help`, { type: 'PLAYING' });
+    client.user.setActivity(` ONLINE | ${config.prefix}help`, { type: 'PLAYING' });
 }
